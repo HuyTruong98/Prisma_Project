c4725c073c401f38d2de5868ed6bc2e08fda2b88
truonghoanghuy98 <truonghoanghuy98@gmail.com>
2023-08-28 14:42:24 +0700

    update

--- index.js
+++ index.js
@@ -20,3 +20,71 @@ app.listen(8080);
 // tìm hiểu cấu trúc model
 // sử dụng 2 khái niệm database first và code first
 // tìm hiểu các truy vấn dữ liệu của prisma
+
+// sử dụng database first
+// yarn prisma generate => làm mới lại file schema.prisma
+
+const { PrismaClient } = require("@prisma/client");
+const model = new PrismaClient();
+
+app.get("/list-user", async (req, res) => {
+  // SELECT * FROM user
+  // sequelize model.user.findAll() => Array object (list)
+
+  const data = await model.user.findMany();
+
+  // sequelize model.user.findOne() => Object (get by Id)
+  // SELECT * FROM user WHERE user_id = 2
+  // const data = await model.user.findFirst({
+  //   where: {
+  //     user_id: 2,
+  //   },
+  // });
+
+  // SELECT * FROM user WHERE full_name LIKE '%a%'
+  // const newData = await model.user.findMany({
+  //   where: {
+  //     full_name: {
+  //       contains: "h",
+  //     },
+  //   },
+  // });
+
+  //JOIN (kết nối 2 bảng) (quan hệ một nhiều)
+  // const newData = await model.food.findMany({
+  //   include: {
+  //     food_type: true,
+  //   },
+  // });
+  //JOIN (kết nối 2 bảng) (quan hệ nhiều nhiều)
+  const newData = await model.like_res.findMany({
+    include: {
+      restaurant: true,
+      user: true,
+    },
+  });
+
+  res.send(newData);
+});
+
+app.post("/create-user", async (req, res) => {
+  const body = req.body;
+  await model.user.create({ data: body });
+
+  // lưu ý truyền dữ liệu mới thông qua thuốc tính data ở prisma create, update
+  res.send("Create success !");
+});
+
+app.put("/update-user/:user_id", async (req, res) => {
+  const body = req.body;
+  const { user_id } = req.params;
+
+  await model.user.update({
+    data: body,
+    where: {
+      user_id: Number(user_id),
+    },
+  });
+
+  res.send("Update success !");
+});
